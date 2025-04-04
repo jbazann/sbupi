17cfe44a1e8c4557312c06102d6199faa4efef99
jbazann <joa7.baz@gmail.com>
2025-04-04 13:09:23 -0300

    Upgrade setup.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
+import mkcert from "vite-plugin-mkcert";
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        logLevel: 'debug'
+    },
     plugins: [
-        tailwindcss()
+        tailwindcss(),
+        mkcert()
     ],
 });
