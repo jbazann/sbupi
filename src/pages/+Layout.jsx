5ffdb7111c23ded44f58245015871d7cddf98de2
jbazann <joa7.baz@gmail.com>
2025-04-21 02:37:00 -0300

    TEMPORARILY turn off React strict mode.

--- src/pages/+Layout.jsx
+++ src/pages/+Layout.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
 import './Layout.css'
 
 export default function Layout({ children }) {
     return (
-        <React.StrictMode>
+        <>
             {children}
-        </React.StrictMode>
+        </>
     )
 }
--- src/pages/+config.js
+++ src/pages/+config.js
@@ -3,7 +3,8 @@ import vikeReact from 'vike-react/config'
 // https://vike.dev/config
 export default {
     // https://vike.dev/head
-    favicon: './icon/favicon.png',
+    favicon: '/icon/favicon.png',
+    reactStrictMode: false, // TODO TEMPORARY REMEMBER
     extends: vikeReact,
     prerender: true,
     ssr: false,
