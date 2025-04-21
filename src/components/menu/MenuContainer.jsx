ff555bb9a379e85aad2d6727e0e15939626e89cd
jbazann <joa7.baz@gmail.com>
2025-04-21 02:42:06 -0300

    Improve MenuContainer classes.

--- src/components/menu/MenuContainer.jsx
+++ src/components/menu/MenuContainer.jsx
@@ -2,7 +2,7 @@ import styles from "./MenuContainer.module.css"
 
 export default function MenuContainer({menu, clean = false}) {
     return <>
-        <div className={clean ? styles.leafContainer : styles.intermediateContainer}>
+        <div className={clean ? styles.bareContainer : styles.scrollableContainer}>
             {menu}
         </div>
     </>
