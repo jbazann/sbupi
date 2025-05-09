2e4232db44cac490d7a6272ea5044be00d1bce03
jbazann <joa7.baz@gmail.com>
2025-05-09 16:24:03 -0300

    Remove deprecated file.

--- src/components/client/clientImports.js
+++
@@ -1,9 +0,0 @@
-// TODO learn more about bundling and imports in general to get rid of this abomination
-export function get(key) {
-    let Component = null
-    switch(key) {
-        case 'Bleh': Component = import('./IgnoranceWrapper.jsx')
-            break
-    }
-    return Component
-}
