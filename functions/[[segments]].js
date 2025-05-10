5a2e2044afb73c6b59c90c4100b5f4a23f144098
jbazann <joa7.baz@gmail.com>
2025-05-10 18:30:34 -0300

    Fix server routing. Sanity refactor.

--- functions/[[segments]].js
+++ functions/[[segments]].js
@@ -1,57 +1,44 @@
-import {deserialize, validatePath} from "../src/lib/routing.js";
-
 export async function onRequest(context) {
-    const { request, env } = context,
-        path = context.functionPath,
-        cookies = cookiesObject(request.headers.get('Cookie') || ""),
-        acceptLanguage = request.headers.get('Accept-Language') || ""
+    if (isExcluded(context.functionPath))
+        return context.env.ASSETS.fetch(context.request)
 
-    if (isExcluded(path)) return await env.ASSETS.fetch(request)
+    const { request, env, functionPath, params } = context,
+        cookies = parseCookies(request),
+        firstSegment = (params.segments || [])[0]?.toLowerCase(),
+        language = request.headers.get('Accept-Language') || "en",
+        isLocalizedPath = isLanguageSegment(firstSegment),
+        htmlPath = isLocalizedPath ? `/${firstSegment}/` : '/',
+        segments = ((isLocalizedPath ? params.segments.slice(1) : params.segments) || [])
+            .map(s => s.toLowerCase())
 
-    let segments = context.params.segments || [],
-        localized = isLanguageSegment(segments[0]),
-        localizedTarget = false
-
-    if (localized) {
-        segments = segments.slice(1)
-    } else if ((localizedTarget =
-        prefersSupportedLanguage(acceptLanguage,cookies))) {
-        return redirect(localizedTarget + path)
+    // Redirect to localized path
+    let localizedTarget = false
+    if (!isLocalizedPath && (localizedTarget = prefersSupportedLanguage(language,cookies))) {
+        return redirect(localizedTarget + functionPath)
     }
-    // At this point, we are fetching the correct, localized HTML,
-    // though a redirect may still need to happen if the path is invalid.
-    // Currently, HTML _must_ be fetched to validate paths.
-    let content = await env.ASSETS.fetch(request)
 
-    let validPath, redirect
-    const extractor = {
-        // This extractor builds the routing data structures
-        // that will be used by the client. They _could_
-        // be extracted for the final routing step, it might
-        // even be more efficient that way, but I already
-        // implemented a different method so whatever.
-        element(element) {
-            const data = element.getAttribute('data-attributes')
-                ?.replace(/&quot;/g, '"')
-            deserialize(JSON.parse(data).data)
-            const r = validatePath(path)
-            validPath = r[0]
-            redirect = r[1]
-        }
+    // Parse valid path segments for routing
+    const validSegments = []
+    let serverPath = await env.ASSETS.fetch('https://assets.local/serverpath.json').then(r => r.json())
+    for (const segment of segments) {
+        if (serverPath[segment]) {
+            validSegments.push(segment)
+            serverPath = serverPath[segment]
+        } else break
     }
-    content = new HTMLRewriter()
-        .on('[data-hydration-root=Router]', extractor)
-        .transform(content)
 
-    if (redirect) return redirect(validPath)
-    // At this point, <segments> was built from a <validPath>
-    // so there is no need to build a new array,
-    // though it may still contain wierd capitalization if no redirection happened
+    // Fetch localized HTML
+    let response = await env.ASSETS.fetch(new Request(
+        // This trims the path from the url, replacing it with htmlPath
+        // This is because Cloudflare seems to only let me have full SPA
+        // or full MPA routing; I guess making sense is beyond web developers
+        replacePath(request.url,htmlPath),
+        request
+    ))
 
-    return setupRouter(
-        new HTMLRewriter(),
-        [...segments.map(s => s.toLowerCase())]
-    ).transform(content)
+    // Set initial client-routing state.
+    return serverRouter(new HTMLRewriter(), validSegments)
+        .transform(response)
 }
 
 const excluded = [
@@ -68,6 +55,11 @@ function isExcluded(path) {
                 if (path.startsWith(prefix)) return true
             }
     }
+}
+
+function replacePath(url, path) {
+    const hostnameIndex = url.indexOf('://') + 3
+    return url.slice(0, url.indexOf('/', hostnameIndex)) + path
 }
 
 function prefersSupportedLanguage(acceptLanguage, cookies) {
@@ -88,7 +80,7 @@ function normalizePath(path) {
 }
 
 function isLanguageSegment(segment) {
-    switch (segment) {
+    switch (segment?.toLowerCase()) {
         case 'es':
             return true
         default:
@@ -102,7 +94,7 @@ function redirect(target) {
     return Response.redirect(target)
 }
 
-function setupRouter(htmlRewriter, segments) {
+function serverRouter(htmlRewriter, segments) {
     let current, previous = 'root'
     segments = segments.reverse()
     while ((current = segments.pop())) {
@@ -141,8 +133,9 @@ function isLanguagePath(str) {
     }
 }
 
-function cookiesObject(cookieStr) {
-    return cookieStr.split(";")
+function parseCookies(request) {
+    return (request.headers.get('Cookie') || "")
+        .split(";")
         .map(str => str.split("="))
         .reduce((acc, curr) => {
             if (curr && curr[2]) {
@@ -157,7 +150,7 @@ function cookiesObject(cookieStr) {
         }, {})
 }
 
-function spanishOverEnglish(ALHeader) {
-    return ALHeader.includes('es') &&
-        (!ALHeader.includes('en') || ALHeader.indexOf('es') < ALHeader.indexOf('en'))
+function spanishOverEnglish(acceptLanguage) {
+    return acceptLanguage.includes('es') &&
+        (!acceptLanguage.includes('en') || acceptLanguage.indexOf('es') < acceptLanguage.indexOf('en'))
 }
