b5963372c8700c0888fc8b2e261311bd18ec20b3
jbazann <joa7.baz@gmail.com>
2025-06-03 14:28:19 -0300

    Introduce event dispatch script.

---
+++ src/components/scripts/OnClickDispatchEventScript.jsx
@@ -0,0 +1,13 @@
+import {useClickHandler} from "@l/hooks.client.js";
+import {key} from "@l/utils.client.js";
+import {devLog, loggedEvent} from "@l/common.shared.js";
+
+export default OnClickDispatchEventScript
+
+function OnClickDispatchEventScript({clickableId,eventName,eventTargetId = ''}) {
+    devLog({clickableId,eventName}, 'EVENT DISPATCH SCRIPT')
+    useClickHandler(clickableId, (elems,event) => {
+        const target = elems[key(eventTargetId)] || window
+        loggedEvent(target,new CustomEvent(eventName))
+    }, {eventTargetId})
+}
