620a996a3b71063be078e8379a193260406f49ff
jbazann <joa7.baz@gmail.com>
2025-06-07 18:35:40 -0300

    Fix hidden HydrationRoot.

--- src/components/system/HydrationRoot.server.jsx
+++ src/components/system/HydrationRoot.server.jsx
@@ -2,7 +2,7 @@ export default function HydrationRoot({comp,data,metadata}) {
     return <>
         <div data-hydration-root={comp} data-attributes={JSON.stringify(data)}
              data-metadata={JSON.stringify(metadata)}
-             role="none" aria-hidden={true} hidden >
+             role="presentation" className="contents" >
         </div>
     </>
 }
