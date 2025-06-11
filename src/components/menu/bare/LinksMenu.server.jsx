356d52a8427fd89b67837192faefaf20e755f6d6
jbazann <joa7.baz@gmail.com>
2025-06-10 23:44:13 -0300

    Fix not-so-small oopsie.

--- src/components/menu/bare/LinksMenu.server.jsx
+++ src/components/menu/bare/LinksMenu.server.jsx
@@ -1,26 +1,27 @@
 import ServerMenu from "@c/layout/menu/server/ServerMenu.server.jsx";
-import BaseButton from "@c/layout/control/BaseButton.shared.jsx";
+import ActionButton from "@c/layout/control/ActionButton.server.jsx";
+import {sid} from "@l/common.shared.js";
 
 export default function LinksMenu({id, parentId, routingKey}) {
     return <>
         <ServerMenu key="LinksMenu" label={"links"}
                     routingKey={routingKey}
                     id={id} parentId={parentId} >
-            <BaseButton key="LinkedIn">
+            <ActionButton id={sid(id,'linkedin')} key="LinkedIn">
                 <a href="https://www.linkedin.com/in/jbazann/" target="_blank">
                     LinkedIn
                 </a>
-            </BaseButton>
-            <BaseButton key="GitHub">
+            </ActionButton>
+            <ActionButton id={sid(id,'github')} key="GitHub">
                 <a href="https://github.com/jbazann" target="_blank">
                     GitHub
                 </a>
-            </BaseButton>
-            <BaseButton key="Twitter">
+            </ActionButton>
+            <ActionButton id={sid(id,'twitter')} key="Twitter">
                 <a href="https://x.com/jbazann_" target="_blank">
                     X/Twitter
                 </a>
-            </BaseButton>
+            </ActionButton>
         </ServerMenu>
     </>
 }
